9d81d5e56cee88c04bdb43615dd4a97c0a270341
quinncerino <quinncerino@gmail.com>
2025-07-15 20:19:53 -0400

    added navigation bar menu with contact page

--- Home.py
+++ Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-#import pandas   #processes data in csv files
 from csv import DictReader
 
 st.set_page_config(layout = 'wide')
@@ -22,7 +21,7 @@ with col2:
 st.write("Below you can find some of the apps and projects I have programmed. Feel free to contact me!")
 
 
-col3, col4 = st.columns(2)
+col3, middle, col4 = st.columns([1.5, 0.5, 1.5])
 
 
 with col3:
@@ -31,7 +30,8 @@ with col3:
         for index, row in enumerate(csv_reader[:4]):
             st.header(row['title'])
             st.write(row['description'])
-            st.image("images/" + row['image'], width=400)
+            st.image("images/" + row['image'])
+            st.write(f"[Source Code]({row['url']})")
 
 
 with col4:
@@ -40,5 +40,6 @@ with col4:
         for index, row in enumerate(csv_reader[4:]):
             st.header(row['title'])
             st.write(row['description'])
-            st.image("images/" + row['image'], width=400)
+            st.image("images/" + row['image'])
+            st.write(f"[Source Code]({row['url']})")
 
